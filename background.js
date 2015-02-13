f5dbf8b16adce62699f50fdaf1228dd70f1cd219
Riki Fridrich <riki@fczbkk.com>
2015-02-13 12:45:37 +0100

    Release v1.0.0

--- background.js
+++
@@ -1,4 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({file: 'content.js'}); 
-  chrome.tabs.insertCSS({file: 'content.css'}); 
-});
--- content.js
+++
@@ -1,8 +0,0 @@
-var cursorElm = document.createElement('div');
-cursorElm.className = 'mouseDoublePointer';
-document.body.appendChild(cursorElm);
-
-document.body.addEventListener('mousemove', function (e) {
-  cursorElm.style.left = (e.pageX + 1) + 'px';
-  cursorElm.style.top = (e.pageY + 1) + 'px';
-});
